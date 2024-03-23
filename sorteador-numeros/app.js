1b40d8d71ad816c9f2453bb83d18eb4886e641ea
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-23 13:26:19 -0300

    update app.js

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+
+}

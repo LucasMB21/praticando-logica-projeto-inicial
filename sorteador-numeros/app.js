ea7388eed548d6720010cb2636e576bb2cf5f737
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-23 18:08:31 -0300

    update app.js

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -8,10 +8,16 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 
 }
 

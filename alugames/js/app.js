7d97315c01f616f085ad48992efa3fa141e2d429
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-23 19:54:21 -0300

    update app.js

---
+++ alugames/js/app.js
@@ -0,0 +1,8 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    alert(nomeJogo.textContent);
+}

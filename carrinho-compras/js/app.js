97707f89e70101c7b1606ce121fae3c1dc3d5cb7
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-29 17:23:39 -0300

    update carrinho compras

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,20 +1,29 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
     //recuperar valores nome do produto , quantidade e valor
     let Produto = document.getElementById('produto').value;
     let nomeProduto = Produto.split('-')[0];
     let valorUnitario = Produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-
+    let quantidade = document.getElementById('quantidade').value;
     //calcular o preço o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
     //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+</section>`
     //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
+  totalGeral = 0;
+  document.getElementById('lista-produtos').innerHTML = '';
+  document.getElementById('valor-total').innerHTML = 'R$ 0';
 
 }

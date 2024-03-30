941204146c3b643155d5649811ccc8f0c32163fe
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-30 20:35:04 -0300

    create ingreso

---
+++ ingresso/js/app.js
@@ -0,0 +1,19 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = document.getElementById('qtd').value;
+
+    if(tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}

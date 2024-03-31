8442aa0c5f7372053373995b7931f3e64d299393
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-31 17:26:00 -0300

    update app.js

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,11 +1,38 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-
+    amigos.push(amigo.value);
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
+}
+
+function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+      if (i == amigos.length - 1) {
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>'
+      } else {
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>'
+      }   
+
+    } 
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        //atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] =
+            [lista[indiceAleatorio], lista[indice -1]];
+    }
 }

693cca1dff4fb29afbb580bf9fbd6853d5b9f5f4
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-31 18:11:39 -0300

    update app.js

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -18,6 +18,11 @@ function adicionar() {
 }
 
 function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
